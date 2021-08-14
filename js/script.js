4e365fbc9315b7c8adf5ca28daaec559ab74f1d2
mac-mini <lexx.mg@gmail.com>
2021-08-14 13:36:58 +0300

    add js burger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+
 'use strict';
+
+$(document).ready(function() {
+  const btnMenu = $('.js-burger'),
+        burgerItem = $('.burger__item'),
+        menu = $('.header-nav'),
+        body = $('body');
+
+  btnMenu.on('click', function(){
+    if(btnMenu.attr('aria-expanded') === 'false') {
+      menu.slideDown(function(){
+        btnMenu.attr('aria-expanded', 'true');
+        burgerItem.addClass('burger--animate');
+      });
+    } else {
+      menu.slideUp(function(){
+        btnMenu.attr('aria-expanded', 'false');
+        burgerItem.removeClass('burger--animate');
+      });
+    }
+  });
+
+  body.on('click', () => {
+    if (btnMenu.attr('aria-expanded') === 'true') {
+      menu.slideUp(function(){
+        btnMenu.attr('aria-expanded', 'false');
+        burgerItem.removeClass('burger--animate');
+      });
+    }
+  });
+});
